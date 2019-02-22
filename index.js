4cce0e27664703a2e0d08509a99943c0304de38e
Paulo Pires de Avila <42159077+Pauloasdf@users.noreply.github.com>
2019-02-22 01:38:26 -0300

    Adição de end points adicionais
    Foram adicionados end points para atualização/exclusão/inserção de conteúdos. 
    Além disso também foi feita a inclusão de end points para inclusão e verificação do login de usuário.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 2000
+const md5 = require('md5');
 
-//CORS 
+//APP 
 app.use(cors());
+app.use(bodyParser.json())
+
+
+//-------------------------------- CONTEUDOS --------------------------------
 
 // GET CONTEUDOS
   function getConteudos(){
@@ -14,14 +20,76 @@ app.use(cors());
   .then(data => data.map( c => c.dataValues))
 }
 
+// CONTEUDOS TELA INICIAL
 app.get('/conteudos',function(req, res, next){ 
     getConteudos()
       .then((d) => res.send(d))
 })
-// END OF GET CONTEUDOS
+
+//CADASTRO CONTEÚDO
+app.post('/form',function(req, res, next){ 
+  const {imagem,titulo,texto} = req.body;
+  sequelize.query("INSERT INTO conteudos (tituloBloco,texto,img) VALUES ('" + titulo + "','" + texto + "','" + imagem + "')",function(err){})
+})
+
+//ATUALIZAÇÃO CONTEÚDO
+app.post('/update',function(req, res, next){ 
+  const {id,imagem,titulo,texto} = req.body;
+  sequelize.query("UPDATE conteudos set tituloBloco = '" + titulo + "', texto = '" + texto + "', img = '" + imagem + "' where conteudos.id = " + id,function(err){})
+})
+
+//EXCLUSÃO CONTEÚDO
+app.post('/delete',function(req, res, next){ 
+  const {id} = req.body;
+  sequelize.query("DELETE from conteudos where conteudos.id = " + id,function(err){})
+})
+
+//-------------------------------- USUARIOS --------------------------------
+
+function getUsuarios(){
+  return Usuarios.findAll({
+    attributes : ['nome', 'email', 'senha']
+  })
+  .then(data => data.map( e => e.dataValues))
+}
 
 
-// Sequelize Configuratins //
+function confirmaUser(req){
+  const {emailLogin,passLogin} = req.body;
+  const senhaCript = md5(passLogin)
+
+  return Usuarios.find({
+    attributes : ['nome', 'email', 'senha','tipo_usuario'],
+    where:{
+      email : emailLogin,
+      senha: senhaCript
+    }
+  })
+  .then(data => data.dataValues)
+}
+
+
+
+//CADASTRO DE USUÁRIO
+app.post('/newuser',function(req, res, next){
+  const {nome,pass,email,} = req.body;
+  sequelize.query("INSERT INTO usuarios (nome,email,senha) VALUES ('" + nome + "','" + email + "', MD5('" + pass + "'))",function(err){})
+})
+
+//BUSCAR USUÁRIO
+app.get('/usuarios',function(req, res, next){
+  getUsuarios()
+      .then((f) => res.send(f))
+})
+
+//CONFIRMAÇÃO LOGIN USUÁRIO
+app.post('/confirmaUser',function(req, res, next){
+  confirmaUser(req)
+  .then((n) => res.send(n))
+})
+
+
+// Sequelize Configurations 
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize('tcc','root','',{
   host:'localhost',
@@ -80,12 +148,37 @@ const Conteudos = sequelize.define('conteudos', {
   }
 })
 
+const Usuarios = sequelize.define('usuarios', {
+  id:{
+    type : Sequelize.INTEGER,
+    primaryKey :true
+  },
+  nome:{
+    type : Sequelize.STRING
+  },
+  email:{
+    type : Sequelize.STRING
+  },
+  pass:{
+    type : Sequelize.INTEGER
+  },
+  tipo_usuario:{
+    type : Sequelize.STRING
+  },
+  alt:{
+    type : Sequelize.STRING
+  },
+  visualizacoes_usuario:{
+    type : Sequelize.INTEGER
+  },
+  createdAt:{
+    type : Sequelize.DATE,
+  },
+  updatedAt:{
+    type : Sequelize.DATE,
+  }
+})
+
 // End of Sequelize Configuratins //
-
-
-
-
-// app.patch('/form', (req, res)).then(res.send(data))
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
